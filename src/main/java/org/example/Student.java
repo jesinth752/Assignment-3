7a4a5007eb144f4fc4f0d7a7110e0b7a25c8996c
jesinth752 <123439338+jesinth752@users.noreply.github.com>
2023-01-27 00:52:28 +0530

    Update Student.java

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -47,7 +47,7 @@ class GPA extends DET
         set=1;
         return val;
     }
-    void record(String recordname)
+    void Checkrecord(String recordname)
     {
 
       if(studname.equals(recordname))
@@ -101,7 +101,7 @@ public class  Student {
         {
             LOGGER.info("Enter student name:");
             String recordname=input.nextLine();
-            call.record(recordname);
+            call.Checkrecord(recordname);
         }
         else
         {
